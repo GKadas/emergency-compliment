d41d85c5a0e693c9b359d14b454f521f552ec47e
“George <kadas.george@gmail.com>
2021-03-05 00:25:32 +0100

    create random compliments

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import React, {useState}from "react"
-import logo from './logo.svg';
 import "./App.css"
 
 function App() {
   const [compliment, setCompliment] = useState("Hello")
-  const [hexCode, setHexCode] = useState("#FFF")
+  const [color, setColor] = useState("lightblue")
 
+  const compliments = [
+    "You're pretty",
+    "You're smart",
+    "You're test1",
+    "You're test2",
+    "You're my heart",
+    "You're my soul"
+  ]
+
+  function giveCompliment() {
+    let item = compliments[Math.floor(Math.random() * compliments.length)];
+    setCompliment(item)
+  }
 
   return (
-    <div className="App" background-color={hexCode}>
+    <div className="App" background-color={color}>
       <header className="App-header">
         <div className="centered">
           <p>
             {compliment}
           </p>
-          <button className="button">Thanks!</button> <button className="button">MEH!</button>
+          <button className="button" onClick={giveCompliment}>Thanks!</button> <button className="button">MEH!</button>
         </div>
       </header>
     </div>
