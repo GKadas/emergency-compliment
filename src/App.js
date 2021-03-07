dd4258532919e5d379669726063b84c1541e91a5
“George <kadas.george@gmail.com>
2021-03-07 19:37:21 +0100

    added CSS, added likes counter, added action in the MEH button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, {useState, useEffect}from "react"
 import randomColor from "randomcolor"
+
+import 'remixicon/fonts/remixicon.css'
+
 import "./App.css"
 
 const COMPLIMENTS_API_URL = "https://raw.githubusercontent.com/GKadas/emergency-compliment/main/src/pool/aoc.json"
@@ -8,6 +11,7 @@ function App() {
   const [compliments, setCompliments] = useState([])
   const [item, setItem] = useState("Hello")
   const [color, setColor] = useState("#282c34")
+  const [count, setCount] = useState(0)
 
 
   useEffect(() => {
@@ -22,7 +26,20 @@ function App() {
         })
   }, [])
 
-  function giveCompliment() {
+  function increment (){
+    setCount(prevCount => prevCount + 1)
+  }
+
+  function giveCompliment () {
+    let item = compliments[Math.floor(Math.random() * compliments.length)];
+    let color = randomColor()
+    increment()
+
+    setColor(color)
+    setItem(item)
+  }
+
+  function saidMEH () {
     let item = compliments[Math.floor(Math.random() * compliments.length)];
     let color = randomColor()
 
@@ -33,13 +50,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header" style={{backgroundColor: color}}>
-        <div className="centered">
+        <div>
+          <i class="ri-heart-3-fill" style={{position: "absolute", top: "8px", left: "8px"}}>{count}</i>
+        </div>
+        <div className="compliment">
           <p>
             {item}
           </p>
-          <div>
+          <div className="buttonArea">
             <button className="button" onClick={giveCompliment}>Thanks!</button>
-            <button className="button">MEH!</button>
+            <button className="button" onClick={saidMEH}>MEH!</button>
           </div>
         </div>
       </header>
@@ -48,3 +68,7 @@ function App() {
 }
 
 export default App;
+
+<div>
+  <i class="ri-admin-fill"></i>
+</div>
